005c1399d2fb218b152265635bdbbac17e4e08c0
wangminli <wongminli@163.com>
2018-02-13 13:16:40 +0800

    提交基础模块

---
+++ page/music/audio.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    current: {
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻',
+      author: '许巍',
+      src: 'http://fangcheng.qiniudn.com/开场白新.mp3',
+    },
+    audioAction: {
+      method: 'pause'
+    }
+  }
+})
